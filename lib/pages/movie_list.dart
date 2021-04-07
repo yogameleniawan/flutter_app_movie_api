2bf365e6f6e3a9b8ca24f6456b9693a321d0e347
yogameleniawan <64576201+yogameleniawan@users.noreply.github.com>
2021-04-07 14:31:05 +0700

    Membuat halaman list Populer Movie

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -9,29 +9,45 @@ class MovieList extends StatefulWidget {
 }
 
 class _MovieListState extends State<MovieList> {
-  String result = "";
+  int moviesCount;
+  List movies;
   HttpService service;
+
+  Future initialize() async {
+    movies = [];
+    movies = await service.getPopularMovies();
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
+  }
 
   @override
   void initState() {
     service = HttpService();
+    initialize();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    service.getPopularMovies().then((value) => {
-          setState(() {
-            result = value;
-          })
-        });
     return Scaffold(
       appBar: AppBar(
         title: Text("Popular Movies"),
       ),
-      body: Container(
-        child: Text(result),
-      ),
+      body: ListView.builder(
+          itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+          itemBuilder: (context, int position) {
+            return Card(
+              color: Colors.white,
+              elevation: 2.0,
+              child: ListTile(
+                title: Text(movies[position].title),
+                subtitle:
+                    Text('Rating = ' + movies[position].voteAverage.toString()),
+              ),
+            );
+          }),
     );
   }
 }
--- lib/service/httpservice.dart
+++ lib/service/httpservice.dart
@@ -1,5 +1,5 @@
-import 'dart:io';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:pemrograman_mobile_week9/models/movie.dart';
 
