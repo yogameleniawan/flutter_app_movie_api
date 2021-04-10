6993d74978be1306234af922cd9a828c7633523a
yogameleniawan <64576201+yogameleniawan@users.noreply.github.com>
2021-04-11 00:13:50 +0700

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,21 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MovieList(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MovieList();
   }
 }
--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pemrograman_mobile_week9/service/httpservice.dart';
 
 import 'movie_detail.dart';
@@ -14,6 +16,11 @@ class _MovieListState extends State<MovieList> {
   int moviesCount;
   List movies;
   HttpService service;
+  String imgPath = 'https://image.tmdb.org/t/p/w500/';
+
+  var listCategory = ["Popular Movies", "Work", "Shopping", "Home"];
+  String _newValue = "No Category";
+  List<String> imgList = [];
 
   Future initialize() async {
     movies = [];
@@ -21,6 +28,10 @@ class _MovieListState extends State<MovieList> {
     setState(() {
       moviesCount = movies.length;
       movies = movies;
+      for (int i = 0; i < 3; i++) {
+        String image = imgPath + movies[i].posterPath;
+        imgList.add(image);
+      }
     });
   }
 
@@ -33,28 +44,131 @@ class _MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
+    String imgPath = 'https://image.tmdb.org/t/p/w500/';
     return Scaffold(
+      backgroundColor: Color(0xFF151C26),
       appBar: AppBar(
-        title: Text("Popular Movies"),
+        backwardsCompatibility: false,
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarColor: Color(0xFF151C26)),
+        backgroundColor: Color(0xFF151C26),
+        brightness: Brightness.dark,
+        centerTitle: true,
+        title: Text(
+          "Popular Movies",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: ListView.builder(
-          itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
-          itemBuilder: (context, int position) {
-            return Card(
-              color: Colors.white,
-              elevation: 2.0,
-              child: ListTile(
-                title: Text(movies[position].title),
-                subtitle:
-                    Text('Rating = ' + movies[position].voteAverage.toString()),
-                onTap: () {
-                  MaterialPageRoute route = MaterialPageRoute(
-                      builder: (_) => MovieDetail(movies[position]));
-                  Navigator.push(context, route);
-                },
-              ),
-            );
-          }),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+                child: Column(
+              children: <Widget>[
+                CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 2.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: imgList
+                      .map((item) => Container(
+                            child: Container(
+                              margin: EdgeInsets.all(5.0),
+                              child: ClipRRect(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  child: Stack(
+                                    children: <Widget>[
+                                      Image.network(item,
+                                          fit: BoxFit.cover, width: 1000.0),
+                                      Positioned(
+                                        bottom: 0.0,
+                                        left: 0.0,
+                                        right: 0.0,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                              colors: [
+                                                Color.fromARGB(200, 0, 0, 0),
+                                                Color.fromARGB(0, 0, 0, 0)
+                                              ],
+                                              begin: Alignment.bottomCenter,
+                                              end: Alignment.topCenter,
+                                            ),
+                                          ),
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10.0, horizontal: 20.0),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                movies[imgList.indexOf(item)]
+                                                    .title,
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 18.0,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Icon(
+                                                    Icons.star,
+                                                    color: Colors.amber,
+                                                  ),
+                                                  Text(
+                                                    movies[imgList
+                                                            .indexOf(item)]
+                                                        .voteAverage
+                                                        .toString(),
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )),
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ],
+            )),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+                  itemBuilder: (context, int position) {
+                    return Card(
+                      color: Colors.white,
+                      elevation: 2.0,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30.0,
+                          backgroundImage: NetworkImage(
+                              imgPath + movies[position].posterPath),
+                          backgroundColor: Colors.transparent,
+                        ),
+                        title: Text(movies[position].title),
+                        subtitle: Text('Rating = ' +
+                            movies[position].voteAverage.toString()),
+                        onTap: () {
+                          MaterialPageRoute route = MaterialPageRoute(
+                              builder: (_) => MovieDetail(movies[position]));
+                          Navigator.push(context, route);
+                        },
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
